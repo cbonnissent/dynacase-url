330a16324da30950a98e688e06d71e54815f9ef8
Jérôme Augé <jerome.auge@anakeen.com>
2012-03-07 10:26:49 +0100

    Version 1.1.0 (closes #2434)
    - PHP beautifier
    - Requires dynacase-platform >= 3.2.0
    - Do not call `app_post URL' in info.xml, as the `URL_post' script does
      nothing

--- Actions/sendurl.php
+++ Actions/sendurl.php
@@ -1,32 +1,25 @@
 <?php
-/**
- * Get DAV session
+/*
+ * HTTP redirect
  *
- * @author Anakeen 2006
- * @version $Id: sendurl.php,v 1.2 2009/01/15 16:42:39 eric Exp $
+ * @author Anakeen
  * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License
- * @package FREEDOM
- * @subpackage DAV
- */
- /**
- */
+ * @package URL
+*/
 
-
-
-include_once("FDL/Class.Doc.php");
-
+include_once ("FDL/Class.Doc.php");
 /**
  * Redirect to url
  * @param Action &$action current action
  * @param string $vid identificator for file <vaultid>-<docid>
  */
-function sendurl(&$action) {
-  $url = $action->getParam("URL_URL");
-
-  if ($url) {
-    header("location: $url");
-    exit;
-  }
-  $action->lay->template=_("no URL parameter set");
-
+function sendurl(&$action)
+{
+    $url = $action->getParam("URL_URL");
+    
+    if ($url) {
+        header("location: $url");
+        exit;
+    }
+    $action->lay->template = _("no URL parameter set");
 }
